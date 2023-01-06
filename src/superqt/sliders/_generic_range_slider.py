c56cf64c3cb231ceee61bc992f0b974c70637c69
Kevin <kevin.martellotti@gmail.com>
2023-01-06 14:15:27 +0100

    try6

--- src/superqt/sliders/_generic_range_slider.py
+++ src/superqt/sliders/_generic_range_slider.py
@@ -275,7 +275,7 @@ class _GenericRangeSlider(_GenericSlider[Tuple], Generic[_T]):
         painter.setBrush(brush)
         for i in range(len(self._optSliderPositions)-1):
             if(i%2!=0):
-                painter.drawRect(QRectF(QPointF(self._optSliderPositions[i], r_bar.top()), QPointF(self._optSliderPositions[i+1], r_bar.bottom())))
+                painter.drawRect(QRectF(QPointF(self._handleRect(i, opt).x(), self._handleRect(i, opt).y()), QPointF(self._handleRect(i+1, opt).x(), self._handleRect(i+1, opt).y())))
 
     def _draw_handle(self, painter: QStylePainter, opt: QStyleOptionSlider):
         if self._should_draw_bar:
