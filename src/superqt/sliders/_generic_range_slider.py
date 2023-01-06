13bb57606975cace30660fd02a9d73da0689c8c4
Kevin <kevin.martellotti@gmail.com>
2023-01-06 16:44:23 +0100

    try 15

--- src/superqt/sliders/_generic_range_slider.py
+++ src/superqt/sliders/_generic_range_slider.py
@@ -276,7 +276,7 @@ class _GenericRangeSlider(_GenericSlider[Tuple], Generic[_T]):
         for i in range(len(self._optSliderPositions)-1):
             if(i%2!=0):
                 with open('penesypollasgordos.txt', 'w') as f:
-                    print(self._handleRect(i, opt).x()+' '+ self._handleRect(i, opt).y()+' '+ self._handleRect(i+1, opt).x()+ ' ' + self._handleRect(i+1, opt).y(), file=f)  # Python 3.x
+                    print(str(self._handleRect(i, opt).x())+' '+ str(self._handleRect(i, opt).y())+' '+ str(self._handleRect(i+1, opt).x())+ ' ' + str(self._handleRect(i+1, opt).y()), file=f)  # Python 3.x
                 painter.drawRect(QRectF(QPointF(self._handleRect(i, opt).x(), self._handleRect(i, opt).y()), QPointF(self._handleRect(i+1, opt).x(), self._handleRect(i+1, opt).y())))
 
     def _draw_handle(self, painter: QStylePainter, opt: QStyleOptionSlider):
