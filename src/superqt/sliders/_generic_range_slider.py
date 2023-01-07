5fef78ce44b68af54adb6b5d89548ce129024a78
Kevin <kevin.martellotti@gmail.com>
2023-01-07 15:33:54 +0100

    try new formula

--- src/superqt/sliders/_generic_range_slider.py
+++ src/superqt/sliders/_generic_range_slider.py
@@ -278,7 +278,12 @@ class _GenericRangeSlider(_GenericSlider[Tuple], Generic[_T]):
             brush.setFinalStop(r_bar.bottomRight())
         painter.setPen(self._style.pen(opt))
         painter.setBrush(brush)
-        painter.drawRect(r_bar)
+        for i in range(len(self._optSliderPositions)-1):
+            if(i%2==0):
+                with open('salidaCustom.txt', 'w') as f:
+                    print(self._handleRect(i, opt).x()+' '+ self._handleRect(i, opt).y()+' '+ self._handleRect(i+1, opt).x()+ ' ' + self._handleRect(i+1, opt).y(), file=f)
+                    print(str(self._handleRect(i, opt).x())+' '+ str(self._handleRect(i, opt).y())+' '+ str(self._handleRect(i+1, opt).x())+ ' ' + str(self._handleRect(i+1, opt).y()), file=f)
+                painter.drawRect(QRectF(QPointF(self._handleRect(i, opt).x(), self._handleRect(i, opt).y()), QPointF(self._handleRect(i+1, opt).x(), self._handleRect(i+1, opt).y())))
 
     def _draw_handle(self, painter: QStylePainter, opt: QStyleOptionSlider):
         if self._should_draw_bar:
